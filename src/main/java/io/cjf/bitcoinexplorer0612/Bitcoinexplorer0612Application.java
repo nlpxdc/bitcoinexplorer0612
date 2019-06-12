371336b4f0531a15ad88308475ab9d0289c16827
chenjiefei <nlpx_dc@hotmail.com>
2019-06-12 19:44:09 +0800

    first init

---
+++ src/main/java/io/cjf/bitcoinexplorer0612/Bitcoinexplorer0612Application.java
@@ -0,0 +1,13 @@
+package io.cjf.bitcoinexplorer0612;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bitcoinexplorer0612Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bitcoinexplorer0612Application.class, args);
+    }
+
+}
---
+++ src/test/java/io/cjf/bitcoinexplorer0612/Bitcoinexplorer0612ApplicationTests.java
@@ -0,0 +1,16 @@
+package io.cjf.bitcoinexplorer0612;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Bitcoinexplorer0612ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
